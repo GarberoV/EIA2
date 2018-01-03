748214ee0be96e855762d0a5091a91da280e4641
veronika <veronika@LAPTOP-NTQIO88O.fritz.box>
2018-01-03 22:52:08 +0100

    A10-Test2

---
+++ A10 Test/A10-Test2.ts
@@ -0,0 +1,15 @@
+/*
+Aufgabe: Aufgabe 10, FormElements: Weihnachtsbaumkonfigurator
+Name: Veronika Garbero
+Matrikel: 256581
+Datum: 30.12.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "file:///C:/Users/veronika/git/EIA3/A10%20Test/A10-Test2.html";
+}
+
+
